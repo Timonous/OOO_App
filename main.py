cf5ecf36fa29ac9c737e3903242741f7cbb6e851
Артём Либенсон <artem.russia.perm@gmail.com>
2023-10-24 00:57:52 +0500

    test pwa

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import socket
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
@@ -10,12 +10,7 @@ def index():
     if 'Mobile' in user_agent:
         return render_template('mobile_index.html')
     else:
-        return render_template('index.html')
+        return render_template('mobile_index.html')
 
 if __name__ == '__main__':
-    # Получение внешнего IP-адреса вайфая
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    print(local_ip)
-    # Запуск Flask на внешнем IP и порту 5000
     app.run(debug=True, host='0.0.0.0')
