a3d9db82df38e64ad0cba6fd64feb3b40517d476
Артём Либенсон <artem.russia.perm@gmail.com>
2023-11-16 16:34:16 +0500

    map-panel design changes

--- static/sw.js
+++ static/sw.js
@@ -4,11 +4,8 @@ self.addEventListener('install', function(event) {
     caches.open('my-cache-v1').then(function(cache) {
       console.log('Cache opened');
       return cache.addAll([
-        '/OOO_App',
-        '/OOO_App/static/templates/login.html',
-        '/OOO_App/static/templates/main_Menu.html',
-        '/OOO_App/static/indexStyles.css',
-        '/OOO_App/static/loginStyles.css'
+        '/OOO_App/static/templates/mobile_index.html',
+        '/OOO_App/static/mobile_indexStyles.css',
       ]);
     })
   );
