8671377c3da1289f1790983ec00a6897ec7c6dd2
Артём Либенсон <artem.russia.perm@gmail.com>
2023-11-18 22:17:52 +0500

    map view added

---
+++ static/js/map.js
@@ -0,0 +1,8 @@
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [58.010046, 56.226531], // Координаты центра карты
+        zoom: 10 // Уровень масштабирования
+    });
+}
