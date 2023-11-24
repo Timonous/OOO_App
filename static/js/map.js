45049d371e5942354a7376000c213e3e5e69dc77
Артём Либенсон <artem.russia.perm@gmail.com>
2023-11-24 11:48:45 +0500

    map координаты Перми

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,6 @@
 ymaps.ready(function () {
     var myMap = new ymaps.Map("map", {
-        center: [55.7558, 37.6176],
+        center: [58.010002, 56.227147],
         zoom: 11,
         controls: [] // Пустой массив для скрытия всех элементов управления
     });
