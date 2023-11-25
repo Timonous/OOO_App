4c29e02bcf1ac0819a5f8474ef7759d482ae84b2
Артём Либенсон <artem.russia.perm@gmail.com>
2023-11-25 15:24:31 +0500

    map js update

--- static/js/map.js
+++ static/js/map.js
@@ -2,6 +2,6 @@ ymaps.ready(function () {
     var myMap = new ymaps.Map("map", {
         center: [58.010002, 56.227147],
         zoom: 11,
-        controls: [] // Пустой массив для скрытия всех элементов управления
+        controls: ['zoomControl']
     });
 });
